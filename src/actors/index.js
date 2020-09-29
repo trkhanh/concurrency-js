ace8968f2b9291e64bb47ff63729d4783994e270
Khanh <tran_khanh@outlook.com>
2020-09-29 14:44:06 +0700

    add waiter actor

--- src/actors/index.js
+++ src/actors/index.js
@@ -9,6 +9,29 @@ const customerActor = {
 
   "put on table": (msg, ctx, _state) =>
     console.log(` ${ctx.self.name} sees " ${msg.food} " appear on the table`),
-  "no pie left": (_msg, ctx, _state) =>
-   console.log(` ${ctx.self.name} sulks…`),
+  "no pie left": (_msg, ctx, _state) => console.log(` ${ctx.self.name} sulks…`),
+};
+
+const waiterActor = {
+  order: (msg, ctx, state) => {
+    if (msg.wants == "pie") {
+      dispatch(state.pieCase, {
+        type: "get slice",
+        customer: msg.customer,
+        waiter: ctx.self,
+      });
+    } else {
+      console.dir(`Don't know how to order ${msg.wants} `);
+    }
+  },
+  "add to order": (msg, ctx) =>
+    console.log(
+      `Waiter adds ${msg.food} to ${msg.customer.name} ${msg.customer.name} 's order`
+    ),
+  error: (msg, ctx) => {
+    dispatch(msg.customer, { type: "no pie left", msg: msg.msg });
+    console.log(
+      `\nThe waiter apologizes to ${msg.customer.name} : ${msg.msg} `
+    );
+  },
 };
